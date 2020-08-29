172187dbb8eef13940d85d61466e6dfb7bb3a7d6
alesss-b <alexbradshaw21977@gmail.com>
2020-08-29 10:22:58 -0500

    Added 'Program_files' folder containing 'src' and 'resources', as well as main java file.

---
+++ Program_files/src/SongWritingHelper.java
@@ -0,0 +1,19 @@
+/** 
+
+Song Writng Helper v.0.0.1
+Creators:
+	Matthew Philip
+	Alex Bradshaw
+Description:
+	This is a program intended to help in the music-creation process by generating a template
+	of a specified "depth". The "depth" can range from general song structure, down to specific
+	melodies and rhythms. There will potentially be other features sucha as an automatic backing
+	track. 
+
+*/
+
+class SongWritingHelper() {
+	public static void main(String args[]) {
+
+	}
+}
